f1a3e4f155297351de2ba8335f386f704ecf684f
Bytez <25850701+Bytezz@users.noreply.github.com>
2022-01-13 01:29:37 +0100

    First commit

---
+++ FadeIntro.gd
@@ -0,0 +1,72 @@
+## Coded by Bytez
+## https://github.com/bytezz/godot-fadeintro
+## ----
+## Intro scene with fading pictures
+## (for Godot Game Engine).
+
+extends Control
+
+signal finished
+
+onready var Background = get_node("Background")
+onready var Picture = get_node("CenterContainer/MarginContainer/Picture")
+onready var HoldTimer = get_node("HoldTimer")
+onready var FadeAnimationPlayer = get_node("FadeAnimationPlayer")
+
+## Pics to show with fades
+export(Array, Texture) var pics
+var picIndex = 0
+## Time for pics to stay still
+export var holdSeconds = 1
+## Length of fade in and fade out
+export var fadeSeconds = 1
+## Background color
+export(Color) var backgroundColor = Color.black
+## Do you want to be able to skip all the pics by pressing ui_accept?
+export var enableSkip = true
+export var skipControl = "ui_accept"
+## The next scene to load after all pics (if [empty], will be just ignored)
+export(PackedScene) var nextScene
+
+var finished = false
+
+
+func _ready():
+	Background.color = backgroundColor
+	
+	if pics!=null and pics.size()>0:
+		Picture.texture = pics[picIndex]
+		FadeAnimationPlayer.playback_speed = FadeAnimationPlayer.get_animation("fadeIn").length / fadeSeconds
+		FadeAnimationPlayer.play("fadeIn")
+
+
+func _on_HoldTimer_timeout():
+	FadeAnimationPlayer.play("fadeOut")
+
+
+func _on_FadeAnimationPlayer_animation_finished(anim_name):
+	if anim_name == "fadeIn":
+		HoldTimer.wait_time = holdSeconds
+		HoldTimer.start()
+	elif anim_name == "fadeOut":
+		if picIndex+1 < pics.size():
+			picIndex+=1
+			Picture.texture = pics[picIndex]
+			FadeAnimationPlayer.play("fadeIn")
+		else:
+			end()
+
+
+func _input(event):
+	if enableSkip:
+		if event.is_action_pressed(skipControl):
+			end()
+
+
+func end():
+	emit_signal("finished")
+	finished = true
+	
+	if nextScene != null:
+		# warning-ignore:return_value_discarded
+		get_tree().change_scene(nextScene.get_path())
---
+++ README.md
@@ -0,0 +1,23 @@
+# Intro fade scene
+
+Intro scene with fading pictures (for Godot Game Engine).
+
+# Features
+
+- Signal on finish
+- Multiple pics
+- Customizable seconds to hold the pic still
+- Customizable seconds for fade animation (in and out)
+- Customizable background color
+- Optionable skip control
+- Optionable scene to load on finish
+
+# Screenshots
+
+![Example](screenshots/example.png  "Example")
+
+![Options](screenshots/options.png  "Options")
+
+# LICENSE
+
+[GPLv3](LICENSE)
